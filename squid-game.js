d8bfe799a8bc0a37ebe13c48bbe935be53c7822a
José Dantas <josedantasjdneto@gmail.com>
2025-03-06 19:11:19 -0300

    first commit

---
+++ squid-game.js
@@ -0,0 +1,306 @@
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function generateDebt() {
+
+    const min = 10000
+    const max = 10000000
+
+    let randomNumber = `${10 ** randomIntFromInterval(Math.log10(min), Math.log10(max))}`
+
+    let debt;
+    if (Math.log10(randomNumber) === 7) {
+        let firstDigit = randomIntFromInterval(1, 9)
+        if (firstDigit === 1)
+            debt = max
+        else
+            debt = parseInt(`${randomIntFromInterval(1, 9)}${randomIntFromInterval(1, 9)}${randomNumber.substring(3)}`)
+    } else
+        debt = parseInt(`${randomIntFromInterval(1, 9)}${randomIntFromInterval(1, 9)}${randomNumber.substring(2)}`)
+
+    return debt
+
+    // if (debt >= 1000000)
+    //     debt = `R$${debt / 1000000}m`
+    // else
+    //     debt = `R$${debt / 1000}k`
+
+}
+
+function generateDesperation(debt) {
+
+    const minDebt = 10000
+    const maxDebt = 10000000
+
+    let normalizedDebt = (debt - minDebt) / (maxDebt - minDebt)
+
+    let desperation = parseFloat((0.5 + normalizedDebt * 1.5).toFixed(1))
+
+    return desperation
+
+}
+
+function generateQuirk() {
+
+    let quirks = ["paranoia", "psychopathy"]
+
+    let quirk = "none"
+
+    let randomNumber = randomIntFromInterval(0, quirks.length - 1)
+
+    if (Math.random() < 0.025)
+        quirk = quirks[randomNumber]
+
+    return quirk
+
+}
+
+class Player {
+
+    constructor(number) {
+        this.number = number
+    }
+
+    gender = Math.random < 0.5 ? "male" : "female"
+    isAlive = true;
+    debt = generateDebt();
+    desperation = generateDesperation(this.debt)
+
+    fitness = randomIntFromInterval(50, 150) / 100
+    strength = randomIntFromInterval(50, 150) / 100
+    intellect = randomIntFromInterval(50, 150) / 100
+
+    quirk = generateQuirk()
+
+    bravery = this.quirk === "paranoia" ? 0.5 : this.quirk === "psychopathy" ? 1.5 : randomIntFromInterval(50, 150) / 100
+    charisma = this.quirk === "paranoia" ? 0.5 : this.quirk === "psychopathy" ? 1.5 : randomIntFromInterval(50, 150) / 100
+    hostility = this.quirk === "paranoia" || this.quirk === "psychopathy" ? 1.5 : randomIntFromInterval(50, 150) / 100
+    empathy = this.quirk === "paranoia" || this.quirk === "psychopathy" ? 0 : randomIntFromInterval(50, 150) / 100
+    loyalty = this.quirk === "paranoia" || this.quirk === "psychopathy" ? 0 : randomIntFromInterval(50, 150) / 100
+    trust = this.quirk === "paranoia" || this.quirk === "psychopathy" ? 0 : randomIntFromInterval(50, 150) / 100
+
+    kill() {
+        this.isAlive = false
+    }
+
+}
+
+function generatePlayers(amount) {
+
+    let players = []
+
+    for (let i = 1; i <= amount; i++) {
+        players[i - 1] = new Player(i)
+    }
+
+    return players
+
+}
+
+function showDeaths(deadPlayers) {
+
+    console.log("---------------------------------DEATHS---------------------------------\n")
+
+    for (let i = 0; i < deadPlayers.length; i++) {
+
+        const player = deadPlayers[i].player
+        const round = deadPlayers[i].roundKilled
+        const cause = deadPlayers[i].cause
+
+        if (cause === "moved")
+            console.log(`Player ${player.number} was eliminated in round ${round}`)
+        else
+            console.log(`Player ${player.number} was eliminated for not reaching the finish line in time`)
+    }
+
+    console.log("\n------------------------------------------------------------------------")
+
+}
+
+function redLightGreenLight(players) {
+
+    const totalDistance = 100
+    let timeLeft = 300
+    let canMove = true
+    let moveTimer = 6
+    let round = 1
+
+    let playersData = []
+    let deaths = []
+
+    function checkLineRecursively(player) {
+
+        let line = []
+
+        let playerBehind;
+
+        do {
+            playerBehind = playersData[player.number].personBehind
+            if (playerBehind)
+                player = playerBehind
+        } while (playerBehind)
+
+        line.unshift(player)
+
+        let playerInFront;
+
+        do {
+            playerInFront = playersData[player.number].personInFront
+            if (playerInFront) {
+                player = playerInFront
+                line.unshift(player)
+            }
+        } while (playerInFront)
+
+        return line
+
+    }
+
+    for (let i = 0; i < players.length; i++) {
+        let player = players[i]
+        if (!player.isAlive)
+            continue
+        playersData[player.number] = { walkSpeed: player.fitness / 10 + 0.6, distanceTraveled: 0, hasFinished: false, pushCooldown: 0, personInFront: false, personBehind: false }
+    }
+
+    console.log("\n----------------------GREEN LIGHT!----------------------")
+
+    for (timeLeft; timeLeft > 0; timeLeft--) {
+        if (canMove && moveTimer > 0)
+            moveTimer--
+        else if (!canMove && moveTimer < 6)
+            moveTimer++
+        else if (moveTimer === 0) {
+            canMove = false
+            console.log("\n-----------------------RED LIGHT!-----------------------\n")
+        } else if (moveTimer === 6) {
+            canMove = true
+            console.log("\n----------------------GREEN LIGHT!----------------------")
+            round++
+        }
+
+        for (let i = 0; i < players.length; i++) {
+
+            let player = players[i]
+            let playerData = playersData[player.number]
+
+            if (playerData.hasFinished || !player.isAlive)
+                continue
+
+            if (playerData.personInFront) {
+
+                if (!playerData.personInFront.isAlive) {
+                    console.log(`Since player ${playerData.personInFront.number} was eliminated, player ${player.number} isn't hiding behind anyone!`)
+                    playersData[playerData.personInFront.number].personBehind = false
+                    playerData.personInFront = false
+                } else if (playerData.distanceTraveled >= totalDistance - 20) {
+                    console.log(`Player ${player.number} has stopped hiding behind player ${playerData.personInFront.number}`)
+                    playersData[playerData.personInFront.number].personBehind = false
+                    playerData.personInFront = false
+                }
+
+            }
+
+            if (playerData.personBehind) {
+                if (!playerData.personBehind.isAlive) {
+                    playersData[playerData.personBehind.number].personInFront = false
+                    playerData.personBehind = false
+                }
+            }
+
+            if (!canMove) {
+                if (Math.random() < 0.5 / 100 && !playerData.personInFront) {
+                    player.kill()
+                    deaths.push({ player: player, roundKilled: round, cause: "moved" })
+                    // console.log(`Player ${player.number} was eliminated!`)
+                } else if ((player.desperation + player.hostility - player.empathy) / 6 > 0.5 && playerData.pushCooldown === 0 && playerData.distanceTraveled < totalDistance - 20) {
+                    if (Math.random() < 1 / 100) {
+
+                        let playerLine = checkLineRecursively(player)
+
+                        let playerPushed;
+
+                        do {
+                            playerPushed = players[randomIntFromInterval(0, players.length - 1)]
+                            if (!playerLine.includes(playerPushed)){
+                                playerPushed = checkLineRecursively(playerPushed)[checkLineRecursively(playerPushed).length-1]
+                            }else if (playerLine.indexOf(player) > 0) {
+                                playerPushed = playersData[player.number].personInFront
+                            }
+                        } while (playerPushed === player || !playerPushed.isAlive || playersData[playerPushed.number].hasFinished || ((playerLine.indexOf(player) === 0) && (playerLine.includes(playerPushed) && playerLine.indexOf(playerPushed) > playerLine.indexOf(player))) || (playerLine.indexOf(player) > 0 && (!playerLine.includes(playerPushed) || playerLine.indexOf(playerPushed) > playerLine.indexOf(player))))
+                    
+                        let pushedLine = checkLineRecursively(playerPushed)
+
+                        for (let i = pushedLine.indexOf(playerPushed); i >= 0; i--) {
+                            playerPushed = pushedLine[i]
+                            playerPushed.kill()
+                            deaths.push({ player: playerPushed, roundKilled: round, cause: "pushed" })
+                            console.log(`Player ${playerPushed.number} was eliminated! (Pushed by player ${player.number})`)
+                        }
+                        playerData.pushCooldown = 6
+                    }
+                }
+            } else {
+                playerData.distanceTraveled += playerData.personInFront && playerData.walkSpeed > playersData[playerData.personInFront.number].walkSpeed ? playersData[playerData.personInFront.number].walkSpeed : playerData.walkSpeed
+                if (playerData.distanceTraveled >= totalDistance) {
+                    playerData.hasFinished = true
+                    // console.log(`Player ${player.number} has reached the finish line! (Time left: ${timeLeft})`)
+                }
+                if ((player.desperation + player.intellect) / 6 > 0.5 && !playerData.personInFront && playerData.distanceTraveled < totalDistance - 20) {
+                    if (Math.random() < 1 / 100) {
+                        let playerToHideBehind;
+                        do {
+                            playerToHideBehind = players[randomIntFromInterval(0, players.length - 1)]
+                        } while (playerToHideBehind === player || !playerToHideBehind.isAlive || playersData[playerToHideBehind.number].hasFinished)
+                        playerData.personInFront = playerToHideBehind
+                        playersData[playerToHideBehind.number].personBehind = player
+                        console.log(`Player ${player.number} is hiding behind player ${playerToHideBehind.number}!`)
+                    }
+                }
+            }
+
+            if (playerData.pushCooldown > 0) {
+                playerData.pushCooldown--
+            }
+        }
+    }
+
+    console.log("\n-----------------------RED LIGHT!-----------------------\n")
+
+    for (let i = 0; i < players.length; i++) {
+        let player = players[i]
+        if (!player.isAlive)
+            continue
+        if (playersData[player.number].distanceTraveled < totalDistance) {
+            player.kill()
+            deaths.push({ player: player, roundKilled: null, cause: "time" })
+            console.log(`Player ${player.number} has died for not reaching on time!`)
+        }
+    }
+
+    // showDeaths(deaths)
+
+}
+
+function getDeathPercentage(players) {
+
+    let deadPlayers = 0
+
+    for (let i = 0; i < players.length; i++) {
+        let player = players[i]
+        if (!player.isAlive)
+            deadPlayers++
+    }
+
+    return parseInt((deadPlayers / players.length) * 100)
+
+}
+
+let playerList = generatePlayers(456)
+
+// console.log(playerList[342])
+
+redLightGreenLight(playerList)
+
+console.log(`\n${getDeathPercentage(playerList)}%`)
